61fe9387ab2aa88eb3ab19183747c366e7eea140
Meth Munindradasa <methsndr@gmail.com>
2020-11-04 19:12:52 +0530

    Finalized main countdown

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+import * as consts from './utils/consts.js';
+import Countdown from './utils/countdown';
+
 function App() {
   return (
-    <div className="App">
-      lol
+    <div>
+      <div className="mainLanding">
+        <img className="left_logo" src={consts.left_log} />
+        <div className="countDown"><Countdown /></div>
+        <img className="main_btui_text" src={consts.main_btui_text}/>
+        <img className="main_back" src={consts.main_back}/>
+        <div className="register_button">Register</div>
+      </div>
+
+      <div className="cdSection">
+        <div className="mainCount">
+          <img className="pLogo" src={consts.btuiPLogo} />
+          <Countdown looks="bigCountdown" />
+        </div>
+        <img className="bgTextureTwo" src={consts.background_texture}/>
+      </div>
     </div>
   );
 }
---
+++ src/utils/consts.js
@@ -0,0 +1,8 @@
+const background_texture = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fbackground-texture.png?alt=media&token=fdffa77f-0a40-499b-9e32-4ed48fde223d";
+const left_log = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fleft-logo.png?alt=media&token=34f08596-a5c0-4910-a929-dea7de07af0b";
+const main_btui_text = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fmain-btui-text.png?alt=media&token=70917f8f-327a-443c-8d7d-e78a63dc4175";
+const right_sold = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fright-sold.png?alt=media&token=1fd22c7c-a250-419e-92aa-cfaad4e3cfe3";
+const main_back = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fmain_back.png?alt=media&token=76fda63f-e2ec-41ad-8959-3f52db9b9120";
+const btuiPLogo = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2FbtuiPLogo.png?alt=media&token=39a46586-24b5-4079-980f-c3adada8ea83";
+
+export { background_texture, left_log, main_btui_text, right_sold, main_back, btuiPLogo }
