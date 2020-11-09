b1e615b97da8430d0127d5a0a4e5621fc3339e5b
Kavienan J <45987371+kavienanj@users.noreply.github.com>
2020-11-09 19:42:47 +0530

    added bootstrap, made basic structure for programwiz

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import * as consts from "./utils/consts.js";
+import Countdown from "./widgets/countdown";
+import SchoolRegProcess from "./views/schoolRegProcess";
+import GuestRegProcess from "./views/guestRegProcess";
 
-import * as consts from './utils/consts.js';
-import Countdown from './widgets/countdown';
-import SchoolRegProcess from './views/schoolRegProcess';
-import GuestRegProcess from './views/guestRegProcess';
+import { analytics } from "./utils/firebase";
 
-import {analytics} from './utils/firebase';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import RegistrationIntro from './views/registrationIntro';
-import Programwiz from './views/programwiz';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import RegistrationIntro from "./views/registrationIntro";
+import Programwiz from "./views/programwiz";
 
 function App() {
   return (
@@ -24,36 +19,77 @@ function App() {
           <div>
             <div className="mainLanding">
               <img alt="" className="left_logo" src={consts.left_log} />
-              <img alt="" className="left_logo_mobile" src={consts.crest_and_logo_mobile} />
-              <div className="countDown"><Countdown /></div>
-              <img alt="" className="main_btui_text" src={consts.main_btui_text}/>
-              <img alt="" className="main_btui_text_mobile" src={consts.main_logo_mobile} />
-              <img alt="" className="main_back" src={consts.main_back}/>
-              <img alt="" className="main_back_mobile" src={consts.main_background_mobile} />
-              <div className="register_button" onClick={()=>{window.location.href="/registration"; analytics.logEvent("register_button_click");}}>Register</div>
+              <img
+                alt=""
+                className="left_logo_mobile"
+                src={consts.crest_and_logo_mobile}
+              />
+              <div className="countDown">
+                <Countdown />
+              </div>
+              <img
+                alt=""
+                className="main_btui_text"
+                src={consts.main_btui_text}
+              />
+              <img
+                alt=""
+                className="main_btui_text_mobile"
+                src={consts.main_logo_mobile}
+              />
+              <img alt="" className="main_back" src={consts.main_back} />
+              <img
+                alt=""
+                className="main_back_mobile"
+                src={consts.main_background_mobile}
+              />
+              <button
+                className="register_button"
+                onClick={() => {
+                  window.location.href = "/registration";
+                  analytics.logEvent("register_button_click");
+                }}
+              >
+                Register
+              </button>
             </div>
 
             {/* <div className="cdSection"> */}
-              <div className="mainCount">
-                <img alt="" className="pLogo" src={consts.btuiPLogo} />
-                <Countdown looks="bigCountdown" />
-              </div>
-              {/* <img alt="" className="bgTextureTwo" src={consts.background_texture}/>
+            <div className="mainCount">
+              <img alt="" className="pLogo" src={consts.btuiPLogo} />
+              <Countdown looks="bigCountdown" />
+            </div>
+            {/* <img alt="" className="bgTextureTwo" src={consts.background_texture}/>
             </div> */}
 
             <div className="infoDiv">
               <img className="pcInfoDiv" alt="" src={consts.info_div_pc} />
-              <img className="mobileInfoDiv" alt="" src={consts.mobile_info_div_one} />
-              <img className="mobileInfoDiv" alt="" src={consts.mobile_info_div_two} />
+              <img
+                className="mobileInfoDiv"
+                alt=""
+                src={consts.mobile_info_div_one}
+              />
+              <img
+                className="mobileInfoDiv"
+                alt=""
+                src={consts.mobile_info_div_two}
+              />
             </div>
-
           </div>
         </Route>
-        <Route exact path="/registration"><RegistrationIntro /></Route>
-        <Route exact path="/registration/school"><SchoolRegProcess /></Route>
-        <Route exact path="/registration/individual"><GuestRegProcess /></Route>
+        <Route exact path="/registration">
+          <RegistrationIntro />
+        </Route>
+        <Route exact path="/registration/school">
+          <SchoolRegProcess />
+        </Route>
+        <Route exact path="/registration/individual">
+          <GuestRegProcess />
+        </Route>
 
-        <Route exact path="/programwiz"><Programwiz /></Route>
+        <Route exact path="/programwiz">
+          <Programwiz />
+        </Route>
       </Switch>
     </Router>
   );
--- src/views/programwiz.js
+++ src/views/programwiz.js
@@ -1,31 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// Use this file for styles
-// MAKE SURE TO USE CLASSES XD
-import './programwiz.css';
+import "./programwiz.css";
 
 const btnClick = () => {
-    console.log("LEMME IN");
-}
+  console.log("LEMME IN");
+};
 
 function Programwiz() {
-    let elem = (
-        <div>
-            <h1>Programwiz</h1>
-            
-            <div className="progRulesDiv">
-                <h2>Rules And Regulations</h2>
-            </div>
+  document.getElementsByTagName("BODY")[0].style["background"] =
+    "linear-gradient(#141e30, #243b55)";
+  document.getElementsByTagName("BODY")[0].style["height"] = "100%";
+  let elem = (
+    <div className="container pwBody">
+      <h1 className="display-1 pwTopic">Programwiz</h1>
+      <hr color="white" />
+      <div className="pwRulesSection">
+        <h2 className="pwRulesH2">Rules And Regulations</h2>
+        <ol className="pwRulesList">
+          <li># Rule 1</li>
+          <li># Rule 2</li>
+          <li># Rule 3</li>
+          <li># Rule 4</li>
+        </ol>
+      </div>
+      <div>
+        <button onClick={btnClick}>Enter Contest</button>
+      </div>
+    </div>
+  );
 
-            <div>
-                <button onClick={btnClick}>Goto Hakkarank</button>
-            </div>
-
-        </div>
-    );
-
-    return elem;
+  return elem;
 }
 
 export default Programwiz;
