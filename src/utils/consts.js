46a55c25e4c771bab7fdb7ff52dd02d43874a653
Meth Munindradasa <methsndr@gmail.com>
2020-11-11 06:43:57 +0530

    Fixed spelling errors

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,6 @@
 const background_texture = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fbackground-texture.png?alt=media&token=fdffa77f-0a40-499b-9e32-4ed48fde223d";
 const left_log = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fleft-logo.png?alt=media&token=34f08596-a5c0-4910-a929-dea7de07af0b";
-const main_btui_text = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fmain-btui-text.png?alt=media&token=70917f8f-327a-443c-8d7d-e78a63dc4175";
+const main_btui_text = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2FWeb%201%20with%20logo%20_deskksk.png?alt=media&token=ac90a4ad-8b5f-48fb-84f2-e3cb9c84ee81";
 const right_sold = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fright-sold.png?alt=media&token=1fd22c7c-a250-419e-92aa-cfaad4e3cfe3";
 const main_back = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fmain_back.png?alt=media&token=76fda63f-e2ec-41ad-8959-3f52db9b9120";
 const btuiPLogo = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2FbtuiPLogo.png?alt=media&token=39a46586-24b5-4079-980f-c3adada8ea83";
@@ -9,7 +9,7 @@ const fancy_logo_fat = "https://firebasestorage.googleapis.com/v0/b/btui-2020.ap
 
 const crest_and_logo_mobile = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fmobile%2Fcrest_and_logo_mobile.png?alt=media&token=f28b091c-63ea-41d8-8a5d-4912035d33f7";
 const main_background_mobile = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fmobile%2Fmain_background_mobile.png?alt=media&token=2bd9b0c4-4688-45bd-8272-e30a6b02d414";
-const main_logo_mobile = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2Fmobile%2Fmain_logo_mobile.png?alt=media&token=bdf901a4-a094-4ac0-9238-48898be52dc7";
+const main_logo_mobile = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2FWeb%201%20mobile.png?alt=media&token=0247ae51-60cd-4873-a04b-302a80137291";
 
 const info_div_pc = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2FinfoDiv_pc.png?alt=media&token=70a499de-83c6-48ad-8ba9-568d05ebd4a9";
 const mobile_info_div_one = "https://firebasestorage.googleapis.com/v0/b/btui-2020.appspot.com/o/assets%2FmobileInfoDiv_one.png?alt=media&token=5caf4621-2aa2-490c-8357-d78c7274ddab";
