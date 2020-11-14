83e7a1631380bb3b36acac72130e8d5f911e8b69
Meth Munindradasa <methsndr@gmail.com>
2020-11-14 11:59:46 +0530

    A huge ass f*cking spice up.  WHOOHOOO!

--- src/widgets/livePage.js
+++ src/widgets/livePage.js
@@ -8,18 +8,23 @@ import {U_BTUI_LOGO} from '../utils/consts';
 import './livePage.css';
 
 function LiveView(props) {
-    document.getElementsByTagName("BODY")[0].style["background"] = "linear-gradient(to right, #232526, #414345)";
-    document.getElementsByTagName("BODY")[0].style["animation"] = "streamGradient 10s ease infinite";
-    document.getElementsByTagName("BODY")[0].style["background-size"] = "400% 400%";
+    // document.getElementsByTagName("BODY")[0].style["background"] = "linear-gradient(to right, #232526, #414345)";
+    document.getElementsByTagName("BODY")[0].style["background"] = "radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%)";
+    document.getElementsByTagName("HTML")[0].style["overflow"] = "hidden";
+    document.getElementsByTagName("HTML")[0].style["height"] = "100%";
+    // document.getElementsByTagName("BODY")[0].style["background-color"] = "rgb(200, 200, 100)";
+    // document.getElementsByTagName("BODY")[0].style["animation"] = "streamGradient 10s ease infinite";
+    // document.getElementsByTagName("BODY")[0].style["background-size"] = "400% 400%";
 
 
     const masterPath = props["liveLocation"];
 
     const [heading, setHeading] = useState("");
-    const [timeTillLive, setTimeTillLive] = useState("");
     const [live, setLive] = useState(false);
     const [description, setDescription] = useState("");
     const [videoID, setVideoID] = useState("");
+
+    const [counter, setCounter] = useState((<p></p>));
 
     const playerOptions = {
         width: (window.innerWidth<700) ? String(window.innerWidth - 100) : '640',
@@ -34,33 +39,39 @@ function LiveView(props) {
     const masterRef = db.collection('meta').doc(masterPath);
     masterRef.onSnapshot(doc=>{
         let data = doc.data();
-
         setDescription(d=>data["description"]);
         setHeading(d=>data["heading"]);
         setLive(d=>data["live"]);
-        setTimeTillLive(d=>data["timeTillLive"]);
         setVideoID(d=>data["videoID"]);
+        if(!data["live"])
+        {
+            setCounter(d=>(<Countdown looks="liveCounter" time={data["timeTillLive"]} />));
+        }
     });
 
-    let counter = live ? <p></p> : (<Countdown looks="liveCounter" time={timeTillLive} />);
-    let status = live ? <h3>Live</h3> : <h3>Live In</h3>
+    let status = live ? <h3>L I V E</h3> : (<div><h3>L I V E</h3><h3>I N</h3></div>);
 
     let elem = (
-        <div className="mainLiveDiv">
-            <div>
-                <h1>{heading}</h1>
+        <div>
+            <div className="mainLiveDiv">
+                <div>
+                    <h1 data-content={heading}>{heading}</h1>
+                </div>
+                <div>
+                    <p>{description}</p>
+                </div>
+                <div>
+                    {status}
+                    {counter}
+                </div>
+                <div className="videoDiv">
+                    <YouTube className="videoPlayer" videoId={videoID} opts={playerOptions} />
+                </div>
+                <img src={U_BTUI_LOGO}/>
             </div>
-            <div>
-                <p>{description}</p>
-            </div>
-            <div>
-                {status}
-                {counter}
-            </div>
-            <div className="videoDiv">
-                <YouTube className="videoPlayer" videoId={videoID} opts={playerOptions} />
-            </div>
-            <img src={U_BTUI_LOGO}/>
+            <div id='stars'></div>
+            <div id='stars2'></div>
+            <div id='stars3'></div>
         </div>
     );
 
